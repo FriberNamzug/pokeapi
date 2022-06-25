b07598711c74e49c4b4ef4b90c871e9fe9cbf3c1
Roberto Friber <75671954+FriberNamzug@users.noreply.github.com>
2022-06-25 18:42:00 -0500

    Style:format was applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { get } from './services';
-
+import { } from 'react-router-dom';
 export const App = () => {
 
   const [data, setData] = useState([{
@@ -9,32 +9,39 @@ export const App = () => {
   }]);
 
   useEffect(() => {
+    const getData = async () => {
+      const resultado = await get()
+      setData(resultado.results);
+      console.log(data);
+    }
     getData()
   }, []);
 
 
-  const getData = async () => {
-    const resultado = await get()
-    setData(resultado.results);
-    console.log(data);
-  }
 
   return (
     <div className="App">
-      <h1>Lista de pokemon</h1>
-      <ul>
+      <div className="flex justify-center items-center m-5">
+        <div className="block p-6 bg-white rounded-lg border border-gray-200 shadow-md w-96">
+          <h1 className="text-3xl text-center">Lista de pokemon</h1>
+          <ul className="border rounded-lg mt-5">
 
-        {data.map((pokemon, index) => {
-          return (
-            <li key={index}>
-              <a href={pokemon.url}>{pokemon.name}</a>
-            </li>
-          )
+            {data.map((pokemon, index) => {
+              return (
+                <li
+                  className='w-full px-4 py-2 border-b border-gray-200 text-center hover:bg-blue-500 hover:text-white'
+                  key={index}
+                >
+                  <p >{pokemon.name}</p>
+                </li>
+              )
 
-        }
-        )}
+            }
+            )}
 
-      </ul>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
