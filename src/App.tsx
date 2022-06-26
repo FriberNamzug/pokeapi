e461dead15b38f87dd1245cce8a2498284a954eb
Roberto Friber <75671954+FriberNamzug@users.noreply.github.com>
2022-06-26 01:22:08 -0500

    feat: Added interface and operation to obtain a specific pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,90 @@
 import React, { useEffect, useState } from 'react';
-import { get } from './services';
+import close from './images/close_black_24dp.svg';
+import { get, getData } from './services';
 import { } from 'react-router-dom';
 export const App = () => {
 
-  const [data, setData] = useState([{
+  interface listaPokemon {
+    name: string;
+    url: string;
+  }
+
+  interface pokemon {
+    name: string;
+    sprites: string;
+    abilities: {
+      ability: {
+        name: string;
+      }
+    }[];
+    expe: number;
+
+
+
+  }
+
+  const [flag, setFlag] = useState(false);
+  const [data, setData] = useState<Array<listaPokemon>>([{
     name: '',
     url: ''
   }]);
+  const [pokemon, setPokemon] = useState<pokemon>({
+    name: "",
+    sprites: "",
+    abilities: [{
+      ability: {
+        name: ""
+      }
+    }],
+    expe: 0
+  })
+
+
 
   useEffect(() => {
     const getData = async () => {
-      const resultado = await get()
+      const resultado = await get();
       setData(resultado.results);
-      console.log(data);
     }
-    getData()
+    getData();
   }, []);
 
-
+  const getPokemon = async (id: number) => {
+    const resultado = await getData(id);
+    if (resultado.status === 200) {
+      setFlag(true);
+      console.log(resultado);
+      setPokemon({
+        name: resultado.data.species.name,
+        sprites: resultado.data.sprites.front_default,
+        abilities: resultado.data.abilities,
+        expe: resultado.data.base_experience
+      })
+    } else {
+      setFlag(false);
+      console.log('error');
+    }
+  }
 
   return (
     <div className="App">
+
+
+      {!flag &&
       <div className="flex justify-center items-center m-5">
         <div className="block p-6 bg-white rounded-lg border border-gray-200 shadow-md w-96">
-          <h1 className="text-3xl text-center">Lista de pokemon</h1>
+            <h1 className="text-3xl text-center">Lista de pokemon</h1>
+
           <ul className="border rounded-lg mt-5">
 
             {data.map((pokemon, index) => {
               return (
                 <li
-                  className='w-full px-4 py-2 border-b border-gray-200 text-center hover:bg-blue-500 hover:text-white'
+                  className='w-full cursor-pointer  px-4 py-2 border-b border-gray-200 text-center hover:bg-blue-500 hover:text-white transition duration-200 ease-in-out'
                   key={index}
+                  onClick={() => getPokemon(index + 1)}
                 >
-                  <p >{pokemon.name}</p>
+                  <p>{pokemon.name}</p>
                 </li>
               )
 
@@ -40,8 +92,9 @@ export const App = () => {
             )}
 
           </ul>
+          </div>
         </div>
-      </div>
+      }
     </div>
   );
 }
